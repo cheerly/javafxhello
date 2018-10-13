506b1b24382e285eb741d8195357ef0a1d9e2136
Mark Raynsford <code@io7m.com>
2018-10-13 17:10:39 +0100

    Initial version

---
+++ src/main/java/com/io7m/javafxhello/Main.java
@@ -0,0 +1,25 @@
+package com.io7m.javafxhello;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public final class Main extends Application
+{
+  private Main()
+  {
+
+  }
+
+  @Override
+  public void start(
+    final Stage stage)
+  {
+
+  }
+
+  public static void main(
+    final String[] args)
+  {
+    launch(args);
+  }
+}
